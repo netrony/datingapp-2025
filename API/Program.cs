55c1905e14ce00b02e5f6ce0615add4f4172c442
Rony.Olazabal <netrony@hotmail.com>
2025-08-31 19:46:44 -0400

    Fin de la seccion 2

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
+
+app.MapControllers();
+
+app.Run();
