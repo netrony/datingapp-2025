0dbaf57c6448097e6738e06965d8ff77dc7545aa
Rony.Olazabal <netrony@hotmail.com>
2025-10-14 20:05:54 -0400

    End  of section 9

--- API/Extensions/AppUserExtensions.cs
+++ API/Extensions/AppUserExtensions.cs
@@ -17,8 +17,9 @@ namespace API.Extensions
                 Id = user.Id,
                 Email = user.Email,
                 DisplayName = user.DisplayName,
+                ImageUrl = user.ImageUrl,
                 Token = tokenService.CreateToken(user)
             };
         }
     }
-}
+}  
