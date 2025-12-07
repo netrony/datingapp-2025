0814428a5d891794fa3688a436a157f50958b679
Rony.Olazabal <netrony@hotmail.com>
2025-12-07 17:52:47 -0400

    End of section 17

---
+++ API/Entities/Connection.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Connection(string connectionId, string userId)
+    {
+        public string ConnectionId { get; set; } = connectionId;
+        public string UserId { get; set;} = userId;
+        // Navigation property
+        public Group Group { get; set; } = null!;
+    }
+}
